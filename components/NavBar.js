158d9d84499c3546733cac7e23e50e007304739d
Moonjong Kim <hushkmj@naver.com>
2023-06-19 13:37:31 +0900

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar(){
+    const router = useRouter();
+    return (
+        <nav>
+            <Link style={{color: router.pathname === "/"? "red" : "blue"}} className="hello" href={"/"}>Home</Link>
+            <Link style={{color: router.pathname === "/about"? "red" : "blue"}} href={"/about"}>About</Link>
+        </nav>
+    )
+}
